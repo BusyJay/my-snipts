7499e0fc25ab899aa0de2a447d0379ca60ae8250
Jay Lee <busyjaylee@gmail.com>
2013-11-06 13:19:25 +0800

    完善MySQL密码重置脚本
    - 空值处理
    - 添加两次密码输入验证
    - 让用户自行关闭已运行的mysqld进程

--- mysql-password-recover.sh
+++ mysql-password-recover.sh
@@ -5,16 +5,32 @@ if [ "$(id -u)" != "0" ]; then
 	echo "This script must be run as root!" 1>&2
 	exit 1
 fi
-read -p "Please input username:" name
-read -s -p "Please input new password:" pass
-tmp_file=`mktemp`.sql
+if [[ `pgrep mysqld` != "" ]]; then
+	echo "Mysqld is running, please turn it off." 1>&2
+	exit 1
+fi
+name=
+pass=
+while [[ "$name" == "" ]]; do
+	read -p "Please input username: " name
+done
+while [[ "$pass" == "" ]]; do
+	read -s -p "Please input new password: " first_pass
+	echo
+	read -s -p "Please input your new password again: " second_pass
+	echo 
+	if [[ "$first_pass" == "$second_pass" ]]; then
+		pass="$first_pass"
+	else
+		echo "Two passwords do not match each other!" 1>&2
+	fi
+done
+tmp_file=/tmp/.please_delete_me.sql
 echo "UPDATE mysql.user SET Password=PASSWORD('$pass') WHERE User='$name';
 FLUSH PRIVILEGES;" > $tmp_file
-cat $tmp_file
 echo 
-kill -15 `pgrep mysqld`
-mysqld_safe --init-file=$tmp_file &
+ping -c 2 127.0.0.1 > /dev/null
+mysqld_safe --init-file=$tmp_file & > /dev/null
 ping -c 10 127.0.0.1 > /dev/null
-kill -15 `pgrep mysqld`
+kill -10 `pgrep mysqld` 2> /dev/null
 rm $tmp_file
-
